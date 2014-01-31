f9cdb0bcabb0cf7b85b83fa17dda62b0a3a393ed
Nathan Neulinger <nneul@neulinger.org>
2014-01-31 09:52:31 -0600

    correct url reference for primary dev location

--- libs/Local/ParallelRun.pm
+++ libs/Local/ParallelRun.pm
@@ -1,6 +1,6 @@
 #
 # This file is licensed under the Perl Artistic License 2.0 - http://www.opensource.org/licenses/artistic-license-2.0
-# Development copy of this package available from: http://svn.unixtools.org/perllib
+# Development copy of this package available from: https://github.com/unixtools/perllib
 # Cross contributions/development maintained in parallel with Missouri S&T/UMRPerl library
 #
 
