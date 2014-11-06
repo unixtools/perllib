4f0732fe49660ed5bc08732ef3f20849f886dae9
Nathan Neulinger <nneul@neulinger.org>
2014-11-06 11:42:23 -0600

    remove unused code from AuthSrv module

--- libs/Local/AuthSrv.pm
+++ libs/Local/AuthSrv.pm
@@ -47,8 +47,6 @@ use Local::UsageLogger;
 @EXPORT = qw(
     AuthSrv_Fetch
     AuthSrv_FetchRaw
-    AuthSrv_Authenticate
-    AuthSrv_Unauthenticate
     AuthSrv_SetPathPrefix
 );
 
@@ -56,11 +54,8 @@ $| = 1;
 
 my $AUTHSRV_CACHE = {};
 
-my $AUTHSRV_ENCRYPT;
 my $AUTHSRV_DECRYPT;
-my $AUTHSRV_DELETE;
-my $AUTHSRV_AUTH;
-my $AUTHSRV_AUTH_EXEC;
+my $AUTHSRV_RAW_DECRYPT;
 
 &AuthSrv_SetPathPrefix();
 
@@ -77,11 +72,8 @@ sub AuthSrv_SetPathPrefix {
         $prefix = "";
     }
 
-    $AUTHSRV_ENCRYPT   = $prefix . "authsrv-encrypt";
-    $AUTHSRV_DECRYPT   = $prefix . "authsrv-decrypt";
-    $AUTHSRV_DELETE    = $prefix . "authsrv-delete";
-    $AUTHSRV_AUTH      = $prefix . "authsrv-auth";
-    $AUTHSRV_AUTH_EXEC = $prefix . "authsrv-exec";
+    $AUTHSRV_DECRYPT     = $prefix . "authsrv-decrypt";
+    $AUTHSRV_RAW_DECRYPT = $prefix . "authsrv-raw-decrypt";
 }
 
 # Begin-Doc
@@ -153,12 +145,12 @@ sub AuthSrv_FetchRaw {
 
         if ( $^O !~ /Win/ ) {
             open( AUTHSRV_FETCH_IN, "-|" )
-                || exec( $AUTHSRV_DECRYPT, $user, $instance );
+                || exec( $AUTHSRV_RAW_DECRYPT, $user, $instance );
         }
         else {
-            open( AUTHSRV_FETCH_IN, "$AUTHSRV_DECRYPT $user $instance|" );
+            open( AUTHSRV_FETCH_IN, "$AUTHSRV_RAW_DECRYPT $user $instance|" );
         }
-        $passwd = join("",<AUTHSRV_FETCH_IN>);
+        $passwd = join( "", <AUTHSRV_FETCH_IN> );
         close(AUTHSRV_FETCH_IN);
 
         open( STDERR, ">&AUTHSRV_SV_STDERR" );
@@ -167,75 +159,6 @@ sub AuthSrv_FetchRaw {
     }
 
     return $AUTHSRV_CACHE->{$user}->{$instance};
-}
-
-# Begin-Doc
-# Name: AuthSrv_Authenticate
-# Type: function
-# Description: authenticates current process into afs using user $user, instance afs
-# Syntax: &AuthSrv_Authenticate(user => $user, keep_ccache => 0/1, use_existing_ccache => 0/1);
-# Comments: $user is optional, will use uid of script owner if not.
-#
-# Note - if you are trying to use AuthSrv from a non-perl script, issue something equivalent
-# to: system("/usr/bin/authsrv-auth yourafsuserid");
-#
-# If keep_ccache is non-zero, will attempt to allocate a kerberos credentials cache and keep
-# it around. This is only useful if the script will be using kerberized command line apps
-# such as ssh/scp. This option should not normally be needed.
-#
-# If use_existing_ccache is non-zero, will attempt to use an already existing ccache. This is
-# probably only of use for systems programming apps, and then not very likely. If you are logged
-# in with a telnet session and use_existing_ccache is non-zero, it will replace your current
-# ccache with the newly authenticated one. use_existing_ccache implies keep_ccache. This
-# option should not normally be needed.
-#
-# Your script should probably call the &AuthSrv_Unauthenticate() routine when it is finished
-# so it will clean up your token and any existing credentials cache. This is especially important
-# if you are using the keep_ccache or use_existing_ccache options.
-#
-# NOTE: This routine should basically be considered deprecated and not needed for anything any more.
-#
-# Returns: non-zero on failure
-# End-Doc
-sub AuthSrv_Authenticate {
-    my (%opts) = @_;
-    my $user = $opts{user};
-    my $keep_ccache         = $opts{keep_ccache}         || 0;
-    my $use_existing_ccache = $opts{use_existing_ccache} || 0;
-
-    if ( !$user ) {
-        $user = &Local_CurrentUser();
-    }
-
-
-    if ( !$use_existing_ccache ) {
-        $ENV{KRB5CCNAME} = "FILE:/tmp/krb5cc_authsrv_u" . $< . "_p" . $$ . "_" . time;
-    }
-    elsif ($use_existing_ccache) {
-        if ( !defined( $ENV{KRB5CCNAME} ) ) {
-            return 1;
-        }
-    }
-
-    system( $AUTHSRV_AUTH, $user );
-
-    if ( !$keep_ccache && !$use_existing_ccache ) {
-        system("kdestroy >/dev/null 2>&1");
-    }
-}
-
-# Begin-Doc
-# Name: AuthSrv_Unauthenticate
-# Type: function
-# Description: clears afs authentication and removes credentials cache
-# Syntax: &AuthSrv_Unauthenticate();
-# Comments: Should be run at end of script (or when done with afs) whenever
-# AuthSrv_Authenticate() is used to clean up tokens that are no longer needed.
-# End-Doc
-sub AuthSrv_Unauthenticate {
-    if ( defined( $ENV{KRB5CCNAME} ) ) {
-        system("kdestroy >/dev/null 2>&1");
-    }
 }
 
 1;
