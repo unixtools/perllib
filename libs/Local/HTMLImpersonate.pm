bf0a8603ab827015c0a7a236e2dad2d2d2397146
Nathan Neulinger <nneul@neulinger.org>
2019-02-16 09:24:11 -0600

    Have javascript sample autodetect the domain

--- libs/Local/HTMLImpersonate.pm
+++ libs/Local/HTMLImpersonate.pm
@@ -16,13 +16,11 @@ Use the following as a bookmarklet in your browser for easily switching users. T
 be placed in your code as early as possible. I generally would recommend putting it right after GetRequest, so
 that all users of REMOTE_USER in the code are covered.
 
-NOTE: Be sure and update the bookmarklet with an appropriate base domain for the cookies.
+Bookmarklet (Prompts for user): <A HREF="javascript:imuser=prompt(&quot;Username&quot;,&quot;&quot;);document.cookie=&quot;REMOTE_USER_IMPERSONATE=&quot;+escape(imuser)+&quot;;domain=.&quot;+escape(document.domain)+&quot;path=/&quot;;location=location;">Impersonate</A>
 
-Bookmarklet (Prompts for user): <A HREF="javascript:imuser=prompt(&quot;Username&quot;,&quot;&quot;);document.cookie=&quot;REMOTE_USER_IMPERSONATE=&quot;+escape(imuser)+&quot;;domain=.example.com;path=/&quot;;location=location;">Impersonate</A>
+JavaScript (Prompts for user): javascript:imuser=prompt("Username","");document.cookie="REMOTE_USER_IMPERSONATE="+escape(imuser)+";domain=."+escape(document.domain)+";path=/";location=location;
 
-JavaScript (Prompts for user): javascript:imuser=prompt("Username","");document.cookie="REMOTE_USER_IMPERSONATE="+escape(imuser)+";domain=.example.com;path=/";location=location;
-
-JavaScript (Hardwired to particular user): javascript:document.cookie="REMOTE_USER_IMPERSONATE=specificuser;domain=.example.com;path=/";location=location;
+JavaScript (Hardwired to particular user): javascript:document.cookie="REMOTE_USER_IMPERSONATE=specificuser;domain=."+escape(document.domain)+";path=/";location=location;
 
 Example:
 
